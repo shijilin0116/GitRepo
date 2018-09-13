a3b20fc5faff2e36ecc723ef24d7838968400af1
Jalon <shijilin0116@126.com>
2018-09-13 08:25:39 +0000

    written test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def wordCount(data):
+	re = {}
+	for i in data:
+		re[i] = re.get(i,0) +1
+	return re
+
+if __name__== "__main__":
+	data = ["ab","cd","d","d"]
+	print ("The result is %s" % wordCount(data))
+
+
+
